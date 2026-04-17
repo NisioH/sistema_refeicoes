d9f13d55e80f7a31349bec712ba280a4c1e98caa
NisioH <nisiohelandeoliveira@gmail.com>
2026-04-17 14:01:33 -0400

    Adicionado paginação e corrigido botão e CSS

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('api/', include('api.urls')),
     path('painel/', include('refeicoes.urls')),
+    path('', RedirectView.as_view(url='painel/', permanent=True)),
 ]
--- refeicoes/views.py
+++ refeicoes/views.py
@@ -2,36 +2,37 @@ from django.db.models import Sum
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistroRefeicaoForm
 from .models import RegistroRefeicao
+from django.core.paginator import Paginator
 
 
 def painel_refeicoes(request):
-    # Começa pegando tudo
-    registros = RegistroRefeicao.objects.all()
+    registros = RegistroRefeicao.objects.all().order_by('-data_consumo', '-id') 
 
-    # 1. Pegando os valores que o usuário digitou na barra de pesquisa (URL)
     data_inicio = request.GET.get('data_inicio')
     data_fim = request.GET.get('data_fim')
     local_busca = request.GET.get('local')
     setor_busca = request.GET.get('setor')
 
-    # 2. Aplicando os filtros "se" o usuário digitou algo
     if data_inicio:
-        registros = registros.filter(data_consumo__gte=data_inicio)  # gte = maior ou igual
+        registros = registros.filter(data_consumo__gte=data_inicio)  
     if data_fim:
-        registros = registros.filter(data_consumo__lte=data_fim)  # lte = menor ou igual
+        registros = registros.filter(data_consumo__lte=data_fim)  
     if local_busca:
         registros = registros.filter(local=local_busca)
     if setor_busca:
-        registros = registros.filter(setor__icontains=setor_busca)  # icontains = que contenha a palavra
+        registros = registros.filter(setor__icontains=setor_busca)  
 
-    # 3. Calculando o Total Gasto APENAS dos itens filtrados
     soma = registros.aggregate(Sum('valor_total'))['valor_total__sum']
-    total_gasto = soma if soma else 0.00  # Se não tiver nada, retorna 0
+    total_gasto = soma if soma else 0.00  
+
+    paginator = Paginator(registros, 7) 
+    numero_pagina = request.GET.get('page') 
+    page_obj = paginator.get_page(numero_pagina)
 
     contexto = {
-        'registros': registros,
+        'page_obj': page_obj,
         'total_gasto': total_gasto,
-        'filtros': request.GET  # Devolvemos isso para os campos não apagarem o que foi digitado!
+        'filtros': request.GET  
     }
     return render(request, 'refeicoes/painel.html', contexto)
 
