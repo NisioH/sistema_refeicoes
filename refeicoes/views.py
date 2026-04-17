ba9ff443e715f0eaab1cadfef0d11d62a15accf2
Nisio Oliveira <nisioholiveira@gmail.com>
2026-04-16 22:22:59 -0400

    Busca por filtros

--- refeicoes/views.py
+++ refeicoes/views.py
@@ -1,12 +1,39 @@
+from django.db.models import Sum
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistroRefeicaoForm
 from .models import RegistroRefeicao
 
 
 def painel_refeicoes(request):
+    # Começa pegando tudo
     registros = RegistroRefeicao.objects.all()
 
-    return render(request, 'refeicoes/painel.html', {'registros': registros})
+    # 1. Pegando os valores que o usuário digitou na barra de pesquisa (URL)
+    data_inicio = request.GET.get('data_inicio')
+    data_fim = request.GET.get('data_fim')
+    local_busca = request.GET.get('local')
+    setor_busca = request.GET.get('setor')
+
+    # 2. Aplicando os filtros "se" o usuário digitou algo
+    if data_inicio:
+        registros = registros.filter(data_consumo__gte=data_inicio)  # gte = maior ou igual
+    if data_fim:
+        registros = registros.filter(data_consumo__lte=data_fim)  # lte = menor ou igual
+    if local_busca:
+        registros = registros.filter(local=local_busca)
+    if setor_busca:
+        registros = registros.filter(setor__icontains=setor_busca)  # icontains = que contenha a palavra
+
+    # 3. Calculando o Total Gasto APENAS dos itens filtrados
+    soma = registros.aggregate(Sum('valor_total'))['valor_total__sum']
+    total_gasto = soma if soma else 0.00  # Se não tiver nada, retorna 0
+
+    contexto = {
+        'registros': registros,
+        'total_gasto': total_gasto,
+        'filtros': request.GET  # Devolvemos isso para os campos não apagarem o que foi digitado!
+    }
+    return render(request, 'refeicoes/painel.html', contexto)
 
 def novo_registro(request):
     if request.method == "POST":
