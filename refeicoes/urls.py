61d413af255db9298c84d0a500424413f9c8bbf1
Nisio Oliveira <nisioholiveira@gmail.com>
2026-04-16 21:55:12 -0400

    Finalizado o CRUD

--- refeicoes/urls.py
+++ refeicoes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.painel_refeicoes, name='painel_refeicoes'),
     path('novo/', views.novo_registro, name='novo_registro'),
+    path('editar/<int:id>', views.editar_registro, name='editar_registro'),
+    path('deletar/<int:id>', views.excluir_registro, name='excluir_registro'),
 ]
--- refeicoes/views.py
+++ refeicoes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistroRefeicaoForm
 from .models import RegistroRefeicao
 
@@ -18,4 +18,24 @@ def novo_registro(request):
         form = RegistroRefeicaoForm()
    
     return render(request, 'refeicoes/novo_registro.html', {'form': form})
+
+
+def editar_registro(request, id):
+    registro = get_object_or_404(RegistroRefeicao, id=id)
+
+    if request.method == 'POST':
+        form = RegistroRefeicaoForm(request.POST, instance=registro)
+        if form.is_valid():
+            form.save()
+            return redirect('painel_refeicoes')
+    else:
+        form = RegistroRefeicaoForm(instance=registro)
+
+    contexto = {'form': form, 'registro': registro}
+    return render(request, 'refeicoes/novo_registro.html', contexto)
+
+def excluir_registro(request, id):
+    registro = get_object_or_404(RegistroRefeicao, id=id)
+    registro.delete()
+    return redirect('painel_refeicoes')
 
