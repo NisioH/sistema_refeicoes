501b478834a9b47592d1c83f3d402f3a39036c3b
NisioHelande <nisiohelande@localhost.localdomain>
2026-08-29 16:36:57 -0400

    Limpeza

--- config/urls.py
+++ config/urls.py
@@ -4,13 +4,9 @@ from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # ROTA OCULTA: Apenas quem souber esse endereço exato acessa o painel nativo do Django
     path('painel-gerencial-secreto/', admin.site.urls),
-
     path('login/', auth_views.LoginView.as_view(template_name='refeicoes/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('painel/', include('refeicoes.urls')),
-
-    # MUDANÇA AQUI: Agora a raiz joga direto para a rota com nome 'login'
     path('', RedirectView.as_view(pattern_name='login', permanent=False), name='raiz'),
 ]
